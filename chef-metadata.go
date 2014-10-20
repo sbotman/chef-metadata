e4beb92a3336fdad2e0e61a1d16dc1f17298e5a0
Sander Botman <sbotman@schubergphilis.com>
2014-10-21 00:55:47 +0200

    moving some parts into functions and making it more simple so we can move it into chef-guard

--- chef-metadata.go
+++ chef-metadata.go
@@ -15,15 +15,16 @@ import (
 )
 
 var (
-	srvPath = flag.String("path", "", "Path to chef client sources")
-	srvIP   = flag.String("address", "127.0.0.1", "IP for the service")
-	srvPort = flag.String("port", "8090", "Port for the service")
-	srvURL  = flag.String("url", "http://your.server.com", "URL for the client download location")
+	srvPath   = flag.String("path", "", "Path to chef client sources")
+	srvIP     = flag.String("address", "127.0.0.1", "IP for the service")
+	srvPort   = flag.String("port", "8090", "Port for the service")
+	srvURL    = flag.String("url", "http://your.server.com", "URL for the client download location")
 )
 
+
+// Add type and functions for the Sort interface
 type files []string
 
-// Add functions for the Sort interface
 func (f files) Len() int {
 	return len(f)
 }
@@ -60,74 +61,81 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Chef Client Distribution Handler v0.1.4")
 }
 
-func downloadHandler(w http.ResponseWriter, r *http.Request) {
+func getURLParamsPaht(r *http.Request) string {
+	return filepath.Join(r.FormValue("p"), r.FormValue("pv"), r.FormValue("m"))
+}
+
+func getTargetDir(r *http.Request) string {
+	params := getURLParamsPaht(r)
+
+	dir, err := filepath.Abs(params)
+        if err != nil {
+                log.Fatal(err)
+        }
+
+       if *srvPath != "" {
+                dir = filepath.Join(*srvPath, params)
+        }
+	return dir
+}
+
+
+func getTargetFile(r *http.Request) string {
+	dir  := getTargetDir(r)
+
 	version := r.FormValue("v")
-	product := r.FormValue("p")
-	prodver := r.FormValue("pv")
-	machine := r.FormValue("m")
-	path := filepath.Join("client", product, prodver, machine)
+        if version == "latest" {
+                version = "."
+        }
+	
+        filelist, _ := filepath.Glob(dir + "/*" + version + "*")
 
-	dir, err := filepath.Abs(path)
-	if err != nil {
-		log.Fatal(err)
+        if filelist != nil {
+                sort.Sort(files(filelist))
+                return filelist[0]
+	} else {
+		return ""
 	}
+}
 
-	if *srvPath != "" {
-		dir = filepath.Join(*srvPath, path)
-	}
+func getTargetURL(target string) string {
+	return *srvURL + "/" + target
+}
 
-	if version == "latest" {
-		version = "."
-	}
 
-	filelist, _ := filepath.Glob(dir + "/*" + version + "*")
+func downloadHandler(w http.ResponseWriter, r *http.Request) {
+	path := getURLParamsPaht(r)
+	dir  := getTargetDir(r)
 
-	if filelist != nil {
-		sort.Sort(files(filelist))
-		target := filelist[len(filelist)-1]
-		targetpath := path + target[len(dir):]
-		targetredirect := *srvURL + "/" + targetpath
-		http.Redirect(w, r, targetredirect, http.StatusFound)
+	targetfile := getTargetFile(r)
+
+	if targetfile != "" {
+		targetpath := path + targetfile[len(dir):]
+        	targeturl  := getTargetURL(targetpath)
+		http.Redirect(w, r, targeturl, http.StatusFound)
 	}
 }
 
 func metadataHandler(w http.ResponseWriter, r *http.Request) {
-	version := r.FormValue("v")
-	product := r.FormValue("p")
-	prodver := r.FormValue("pv")
-	machine := r.FormValue("m")
-	path := filepath.Join("client", product, prodver, machine)
+	path := getURLParamsPaht(r)
+        dir  := getTargetDir(r)
 
-	dir, err := filepath.Abs(path)
-	if err != nil {
-		log.Fatal(err)
-	}
+        targetfile := getTargetFile(r)
 
-	if *srvPath != "" {
-		dir = filepath.Join(*srvPath, path)
-	}
+	if targetfile != "" {
+        	targetpath := path + targetfile[len(dir):]
+        	targeturl  := getTargetURL(targetpath)
 
-	if version == "latest" {
-		version = "."
-	}
-
-	filelist, _ := filepath.Glob(dir + "/*" + version + "*")
-
-	if filelist != nil {
-		sort.Sort(files(filelist))
-		target := filelist[len(filelist)-1]
-		targetpath := path + target[len(dir):]
-
-		data, err := ioutil.ReadFile(target)
+		data, err := ioutil.ReadFile(targetfile)
 		if err != nil {
 			log.Fatal(err)
 		}
 
 		targetmd5 := md5.Sum(data)
-		targetsha5 := sha256.Sum256(data)
+		targetsha := sha256.Sum256(data)
 		data = nil
 
-		fmt.Fprintf(w, "url %s md5 %x sha256 %x", *srvURL+"/"+targetpath, targetmd5, targetsha5)
+		fmt.Fprintf(w, "url %s md5 %x sha256 %x", targeturl, targetmd5, targetsha)
 	}
 }
 
